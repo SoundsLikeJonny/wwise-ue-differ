4f50cd24867132d257eeaa7df0fd6f12edd76526
Jon <jonnythejet@gmail.com>
2024-05-13 13:01:21 -0700

    - Setting custom theme to dark_cyan

--- src/ui/theme/theme.py
+++ src/ui/theme/theme.py
@@ -1,14 +1,11 @@
 from PySide6.QtWidgets import QApplication
 from qt_material import apply_stylesheet, list_themes
 
-
 def set_default_theme(app: QApplication):
     app.setStyleSheet('')
 
-
 def set_ose_theme(app: QApplication):
-    apply_stylesheet(app, theme='dark_yellow.xml', extra=Defaults.EXTRA)
-
+    apply_stylesheet(app, theme='dark_cyan.xml', extra=Defaults.EXTRA)
 
 class Defaults:
     EXTRA = {'density_scale': '-2'}
